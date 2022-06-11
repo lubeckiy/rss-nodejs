b97c3b2e3a7ef2e207d063b1744b2fc841775105
Sergey Lubeckiy <s.lubeckiy@gmail.com>
2022-06-12 02:50:50 +0300

    feat: implement readline

--- start.js
+++ start.js
@@ -1,13 +1,39 @@
-import { getUserName } from "./lib/getUserName.js"
+import { getUserName } from './lib/getUserName.js'
+import { printMessage, clearTerminal } from './lib/message.js'
+import { validateCommand } from './lib/validateCommand.js'
+import readline from 'readline'
 
 Error.stackTraceLimit = 0
 const args = process.argv.slice(2)
 
 const { userName, errorMessage } = getUserName(args)
+
 try {
     if (userName)  {
-        console.log(`Welcome to the File Manager, ${userName}`)
-        console.log(`You are currently in: ${process.cwd()}`);
+        clearTerminal()
+        printMessage('message', `Welcome to the File Manager, ${userName}`)
+        printMessage('message', `You are currently in: ${process.cwd()}`)
+
+        const rl = readline.createInterface({
+            input: process.stdin, 
+            output: process.stdout,
+        })
+
+        rl.on('SIGINT', () => {
+            printMessage('message', `Thank you for using File Manager, ${userName}`)
+            process.exit(1)
+        });
+
+        function commandLine(promt) {
+            rl.question(promt, (command) => {
+                validateCommand(command, userName)
+                rl.write(`The command received:  ${command}\n`)        
+                commandLine(promt)
+            })
+        }
+
+        commandLine('> ')
+
     } else {
         throw Error(errorMessage)
     }
