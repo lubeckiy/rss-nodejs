11f6fdcd64e2e00874937179b0608a1d5c086dd2
lubeckiy <s.lubeckiy@gmail.com>
2022-06-19 02:31:44 +0300

    fix: get pathparametr from url

--- handlers/getHandler.js
+++ handlers/getHandler.js
@@ -1,18 +1,21 @@
 import usersDB from "../inmemoryDB.js"
 import * as lib from "../lib/lib.js"
+import url from 'node:url'
 
-export const getHandler = ( id ) =>{
+export const getHandler = ( request, response, serverURL) =>{
       let stCode
       const ctType = lib.contentType.json
       let body
+      const id = request.url.split('/')[3] ? request.url.split('/')[3] : null
+ 
       try{
             if (id) {
                   const user = usersDB.find(user => user.id === id)
                   stCode = user ? lib.statusCode.OK : lib.statusCode.NotFound
-                  body = JSON.stringify(user)
+                  body = user
             } else {
                   stCode = lib.statusCode.OK
-                  body = JSON.stringify(usersDB)
+                  body = usersDB
             }
       } catch(error){
             stCode = lib.statusCode.InternalServerError
